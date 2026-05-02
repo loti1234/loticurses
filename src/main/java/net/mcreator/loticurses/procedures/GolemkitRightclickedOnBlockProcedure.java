431ac5f07c989f0de1a2c838a6bae79d0a5b559b
Gokentoo <Kenji DESTOUCHES@DESKTOP-U5M07TS.numericable.fr>
2026-05-02 19:49:13 +0200

    gggggggggg

--- src/main/java/net/mcreator/loticurses/procedures/GolemkitRightclickedOnBlockProcedure.java
+++ src/main/java/net/mcreator/loticurses/procedures/GolemkitRightclickedOnBlockProcedure.java
@@ -1,6 +1,18 @@
 package net.mcreator.loticurses.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 
 public class GolemkitRightclickedOnBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
